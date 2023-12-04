d7830388955da7e8a6054b36558c4deef09b7b8c
Emil Korudzhiev <e.korudzhiev@abv.bg>
2023-12-04 22:39:32 +0200

    Done some refactoring.

--- Backend/src/user/user.controller.ts
+++ Backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, NotFoundException, Param} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('api/v1/user')
@@ -8,5 +8,17 @@ export class UserController {
   @Get()
   getAllUsers() {
     return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    const userId = parseInt(id, 10);
+    const user = this.userService.getUserById(userId);
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+
+    return user;
   }
 }
